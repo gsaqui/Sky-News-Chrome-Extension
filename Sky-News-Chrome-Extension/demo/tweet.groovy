d9b14480c82117d10be576c8ed941c0c51028773
Glenn Saqui <saqui@unknown-e0-f8-47-29-30-08.config>
2011-05-24 07:11:07 +0100

    Adding all files

---
+++ Sky-News-Chrome-Extension/demo/tweet.groovy
@@ -0,0 +1,29 @@
+
+def stories = []
+stories << "Obama praises 'extraordinary' Irish"
+stories << "Alien steals car from cowboy"
+stories << "Volcanic ash cloud threat hits airline shares"
+stories << "Canada once again better than USA"
+stories << "Americans admit they aren't as cool as Canadian"
+
+def json = new File('/Users/saqui/workspace/extension/demo/testData.json')
+
+def output = new File('output')
+
+json.eachLine(){line, number ->
+    Random random = new Random()
+    def randomNum = random.nextInt(5)
+	def time = new Date()
+	def timeStr = time.format('H:mm:ss');
+
+    if(line.contains('text')){
+        output << """"text":"${stories[randomNum]}","""
+    } else     if(line.contains('id_str')){
+        output << """"id_str":"${time.getTime()}" """
+    } else{
+        output << line
+    }
+    output << '\n'    
+}
+json.setText(output.getText())
+output.delete()
