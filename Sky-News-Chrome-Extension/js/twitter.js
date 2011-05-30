bf6610e2bc9e3fc07ca14a53dd1f595403c1cb7f
Glenn Saqui <saqui@unknown-e0-f8-47-29-30-08.config>
2011-05-30 14:28:37 +0100

    Moved twitter stuff into it's own js file

---
+++ Sky-News-Chrome-Extension/js/twitter.js
@@ -0,0 +1,92 @@
+; (function($) {
+    // secure $ jQuery alias
+    $.fn.twitter = function(options) {
+        function parseTwitter(twitter) {
+			console.log('hi');
+            var $tmpl = $('#template');
+            // console.log('date is ', twitter[0].created_at, this.timeAgo(twitter[0].created_at))
+            // add twitter links
+            $('#news-items').jqoteapp($tmpl, {
+                tweet: twitter[0].text,
+                time: timeAgo(twitter[0].created_at)
+            });
+        }
+
+        function show() {
+            console.log('twitter popup - inside of show');
+            $.ajax({
+                type: "GET",
+                url: "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=SkyNewsBreak",
+                // url: "demo/testData.json",
+                dataType: "json",
+                success: parseTwitter
+            });
+        }       
+
+        /**
+		  *  This was taken from the twitter website - I didn't write it
+	      * relative time calculator
+	      * @param {string} twitter date string returned from Twitter API
+	      * @return {string} relative time like "2 minutes ago"
+	      */
+        function timeAgo(dateString) {
+            var rightNow = new Date();
+            var then = new Date(dateString);
+
+            var diff = rightNow - then;
+
+            var second = 1000,
+            minute = second * 60,
+            hour = minute * 60,
+            day = hour * 24,
+            week = day * 7;
+
+            if (isNaN(diff) || diff < 0) {
+                return "";
+                // return blank string if unknown
+            }
+
+            if (diff < second * 2) {
+                // within 2 seconds
+                return "right now";
+            }
+
+            if (diff < minute) {
+                return Math.floor(diff / second) + " seconds ago";
+            }
+
+            if (diff < minute * 2) {
+                return "about 1 minute ago";
+            }
+
+            if (diff < hour) {
+                return Math.floor(diff / minute) + " minutes ago";
+            }
+
+            if (diff < hour * 2) {
+                return "about 1 hour ago";
+            }
+
+            if (diff < day) {
+                return Math.floor(diff / hour) + " hours ago";
+            }
+
+            if (diff > day && diff < day * 2) {
+                return "yesterday";
+            }
+
+            if (diff < day * 365) {
+                return Math.floor(diff / day) + " days ago";
+            }
+
+            else {
+                return "over a year ago";
+            }
+
+        };
+		show();
+
+    };
+    //end of twitter
+})(jQuery);
+// confine scope
