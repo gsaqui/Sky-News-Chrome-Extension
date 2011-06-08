bdfc91e52707d358e4f72629f22c64b8e61e7f4f
Glenn Saqui <saqui@unknown-e0-f8-47-29-30-08.config>
2011-06-08 22:54:17 +0100

    images

--- Sky-News-Chrome-Extension/js/globals.js
+++ Sky-News-Chrome-Extension/js/globals.js
@@ -1,6 +1,5 @@
 var globals = {
-//    var url = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=SkyNewsBreak",
-	url : "demo/testData.json",
-	waitTime:1000
- 
+    url : "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=SkyNewsBreak",
+    // url : "demo/testData.json",
+	waitTime:30000  // 30 seconds
 }
